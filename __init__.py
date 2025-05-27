08a96b6109ba4e5125e7711fb938074c6e6ab312
MUHAMMED ADNAN <73103641+mr-adnan-adu@users.noreply.github.com>
2025-05-27 09:33:34 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+https://github.com/mr-adnan-adu/telegram-ebooks-bot/tree/1/
+├── __init__.py          # Root level
+├── bot.py
+├── config/
+│   ├── __init__.py      # For config package
+│   └── .env
+└── requirements.txt
